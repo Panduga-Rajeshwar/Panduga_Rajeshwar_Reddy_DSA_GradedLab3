a27addcdd17401001970e736d3fa52cd92c897ab
Panduga-Rajeshwar <153173482+Panduga-Rajeshwar@users.noreply.github.com>
2024-02-06 05:02:35 +0530

    Create Node.java

---
+++ SumPair_BST/Node.java
@@ -0,0 +1,15 @@
+package SumPair;
+
+public class Node {
+	Node left;
+	int data;
+	Node right;
+	
+	public Node() {}
+	public Node(int data)
+	{
+		this.data=data;
+		this.left=this.right=null;
+	}
+
+}
